e9460b6c609d564b80d65ede46fe8d026ba4ddbb
camilo ariza <camiloariza594@gmail.com>
2025-02-13 11:19:16 -0500

    primera version

---
+++ py/number one.py
@@ -0,0 +1,9 @@
+#imprirmir la cantidad de numeros es su lado opuesto
+
+num=1
+
+while num!=0:  
+    num=int(input("introduzca el numero Numero"))
+    if num==0: break
+    else:
+        print(f'-{num}')
